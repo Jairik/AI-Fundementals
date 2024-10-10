4d831a67c3e1b7df9c9bb414f8d396c7a6b4b2a6
Jairik <jairikmc@gmail.com>
2024-10-10 19:29:29 -0400

    Began implementing search functions

--- Graph-Path-Search/GraphPathSearch.py
+++ Graph-Path-Search/GraphPathSearch.py
@@ -3,6 +3,18 @@
 import timeit
 from uninformedsearch import uninformedsearch
 
+# Added an addEdge function for expandability
+def addEdge(city1: str, city2: str, weight: int):
+    if city1 not in cities:
+        cities[city1] = {}
+    if city2 not in cities:
+        cities[city2] = {}
+    # Add the edge from city 1 to city 2
+    cities[city1][city2] = weight
+    # Add the edge from city 2 to city 1
+    cities[city1][city2] = weight
+
+# Hardcoding the provided cities graph
 cities = {
     'Buffalo': {'Boston': 450, 'Pittsburg': 219},
     'Boston': {'Buffalo': 450, 'New York': 216},
@@ -16,30 +28,35 @@ cities = {
     'Washington DC': {'Baltimore': 45, 'Salisbury': 116,
                       'Richmond': 110},
     'Salisbury': {'Philadelphia': 138, 'Baltimore': 117,
-                  'Washingtom DC': 116, 'Norfolk': 132},
+                  'Washington DC': 116, 'Norfolk': 132},
     'Richmond': {'Washington DC': 110, 'Norfolk': 93},
     'Norfolk': {'Richmond': 93, 'Salisbury': 132}
 }
 
 # Getting start and end cities from user
+'''
 startCity = input("Enter Start City: ")
 while startCity not in cities:
     startCity = input("ERROR - Start City not found. Please try again: ")
 endCity = input("Enter End City: ")
 while endCity not in cities:
     endCity = input("ERROR - End city not found. Please try again: ")
+'''
+# Hardcoded for debugging purposes
+startCity = 'Boston'
+endCity = 'Salisbury'
 
 # Create uninformedsearch object to use searches in module
 s = uninformedsearch()
 
 # Breadth First Search
 execution_time1 = timeit.timeit(lambda: s.bfs(cities, startCity, endCity, pr_path=True, pr_totDist=True), number=1)
-print("Execution time: ", execution_time1)
+print("Execution time for BFS: ", round((1000000 * execution_time1), 3), " microseconds\n")
 
 # Depth First Search
 execution_time2 = timeit.timeit(lambda: s.dfs(cities, startCity, endCity, pr_path=True, pr_totDist=True), number=1)
-print("Execution time: ", execution_time2)
+print("Execution time for DFS: ", round((1000000 * execution_time2), 3), " microseconds\n")
 
 # Uniform Cost Search
 execution_time3 = timeit.timeit(lambda: s.ucs(cities, startCity, endCity, pr_path=True, pr_totDist=True), number=1)
-print("Execution time: ", execution_time3)
+print("Execution time for UCS: ", round((1000000 * execution_time3), 3), " microseconds\n")
--- Graph-Path-Search/uninformedsearch.py
+++ Graph-Path-Search/uninformedsearch.py
@@ -14,13 +14,53 @@ class uninformedsearch:
     
     '''Breadth First Search'''
     def bfs(self, cities: dict[str, dict[str, int]], start: str, end: str, pr_path: bool, pr_totDist: bool):
-        pass
+        visited = [start]
+        queue = [start]
+        total_distance = 0
+        while queue:
+            m = queue.pop(0)
+            if pr_path and total_distance == 0:
+                print(m, end=" ")
+            else:
+                print("->", m, end=" ")
+            if m in cities:
+                if m is end:
+                    break
+                for neighbor in cities[m]:
+                    if neighbor not in visited:
+                        visited.append(neighbor)
+                        queue.append(neighbor)
+                        total_distance += cities[m][neighbor]  # Add the cost to total distance
+        
+        if pr_totDist:
+            print("\nTotal BFS Distance: ", total_distance)
     
-    
+
     
     '''Depth First Search'''
     def dfs(self, cities: dict[str, dict[str, int]], start: str, end: str, pr_path: bool, pr_totDist: bool):
-        pass
+        stack = [start]
+        if pr_path:
+            print(start, end=" ")
+        visited = set()
+        total_distance = 0
+        while stack:
+            current = stack.pop()
+            if current == end: # Check if current city is target
+                    if pr_totDist:
+                        print("\nTotal DFS Distance: ", total_distance)
+                    return
+            if current not in visited:
+                visited.add(current)
+                # Check each neighbor and it's weight
+                for neighbor, weight in reversed(cities[current].items()):
+                    if neighbor not in visited:
+                        if pr_path:
+                            if pr_path:
+                                print("->", neighbor, end=" ")
+                        total_distance += weight
+                        stack.append(neighbor)
+                    
     
     
     
