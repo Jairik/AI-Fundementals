55a880f717225a5f9d8041f59f83a3f8e14e12e4
Jairik <jairikmc@gmail.com>
2024-10-10 00:17:14 -0400

    Implemented function skeletons
    Methodology is all set up now, just need to implement searches using dict of dicts as data structure.

--- Graph-Path-Search/GraphPathSearch.py
+++ Graph-Path-Search/GraphPathSearch.py
@@ -1,5 +1,10 @@
 ''' - Main function - JJ McCauley - '''
 
+import timeit
+from uninformedsearch import bfs
+from uninformedsearch import dfs
+from uninformedsearch import ucs
+
 cities = {
     'Buffalo': {'Boston': 450, 'Pittsburg': 219},
     'Boston': {'Buffalo': 450, 'New York': 216},
@@ -17,3 +22,15 @@ cities = {
     'Richmond': {'Washington DC': 110, 'Norfolk': 93},
     'Norfolk': {'Richmond': 93, 'Salisbury': 132}
 }
+
+# BFS
+execution_time = timeit.timeit(lambda: bfs(cities, True, True), number=1)
+print("Execution time: ", execution_time, " seconds")
+
+# DFS
+execution_time = timeit.timeit(lambda: dfs(cities, True, True), number=1)
+print("Execution time: ", execution_time, " seconds")
+
+# UCS
+execution_time = timeit.timeit(lambda: ucs(cities, True, True), number=1)
+print("Execution time: ", execution_time, " seconds")
--- Graph-Path-Search/uninformedsearch.py
+++ Graph-Path-Search/uninformedsearch.py
@@ -1,9 +1,31 @@
 ''' - Defining the search functions - 
         JJ McCauley - 10/9/24 '''
         
-#TODO: Make Module or something, BFS+DFS+UCS
+#TODO: BFS+DFS+UCS algorithms implementation
 
 class uninformedsearch:
     
-    def bfs(self):
+    '''BFS Search
+    Parameters: cities- Dict of dicts (graph)
+    pr_path- Boolean for printing the path
+    pr_totDist- Boolean for printing the total distance of path'''
+    def bfs(self, cities, pr_path, pr_totDist):
+        pass
+    
+    
+    
+    '''DFS Search
+    Parameters: cities- Dict of dicts (graph)
+    pr_path- Boolean for printing the path
+    pr_totDist- Boolean for printing the total distance of path'''
+    def dfs(self, cities, pr_path, pr_totDist):
+        pass
+    
+    
+    
+    '''UCS Search
+    Parameters: cities- Dict of dicts (graph)
+    pr_path- Boolean for printing the path
+    pr_totDist- Boolean for printing the total distance of path'''
+    def ucs(self, cities, pr_path, pr_totDist):
         pass
